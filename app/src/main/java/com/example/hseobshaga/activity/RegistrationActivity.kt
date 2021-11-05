8a2baf67f3b4dd94362ec13c6a48d3d00d5b6bf8
fatalem0 <madchaterrer@gmail.com>
2021-11-05 18:13:48 +0300

    add navigation

--- app/src/main/java/com/example/hseobshaga/activity/RegistrationActivity.kt
+++ app/src/main/java/com/example/hseobshaga/activity/RegistrationActivity.kt
@@ -19,6 +19,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import java.lang.Exception
 
+
 class RegistrationActivity : AppCompatActivity() {
 
     private var data = ArrayList<String>()
